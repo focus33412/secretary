df859554c9055d7eae3a1c28c6b81f44487bfd4b
Стас Дубников <Stas.dubn@gmail.com>
2024-01-15 18:34:33 +0300

    логин

--- secretary/secretweb/app.py
+++ secretary/secretweb/app.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_sqlalchemy import SQLAlchemy
+# app.py
+
+from flask import Flask, render_template, request, redirect, url_for, session
+import pyodbc
 
 app = Flask(__name__)
-app.secret_key = 'qwerty1' # Установите ваш секретный ключ для сессии
+app.secret_key = 'qwerty1'
 
-# Конфигурация подключения к базе данных
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://FOCUS\\SQLEXPRESS/secretary?driver=ODBC+Driver+17+for+SQL+Server?Trusted_Connection=yes'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+connection = pyodbc.connect(
+    'Driver={SQL Server};'
+    'server=FOCUS\\SQLEXPRESS;'
+    'Database=secretary;'
+    'Trusted_Connection=yes;'
+)
 
 # Определение модели для таблицы 'incorect'
-class Incorect(db.Model):
-    __tablename__ = 'incorect'
-    id = db.Column(db.Integer, primary_key=True)
-    login_account = db.Column(db.String(100))
-    password_account = db.Column(db.String(100))
-    FIO = db.Column(db.String(100))
+class Incorect:
+    def __init__(self, id, login_account, password_account, FIO):
+        self.id = id
+        self.login_account = login_account
+        self.password_account = password_account
+        self.FIO = FIO
+
+def execute_query(query, params=None, fetchone=False):
+    cursor = connection.cursor()
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+
+    if fetchone:
+        result = cursor.fetchone()
+    else:
+        result = cursor.fetchall()
+
+    cursor.close()
+    return result
 
 @app.route('/')
 def index():
@@ -27,17 +46,25 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    user = Incorect.query.filter_by(login_account=username, password_account=password).first()
-    if user:
-        return redirect(url_for('dashboard', fio=user.FIO))
+    query = "SELECT * FROM incorect WHERE login_account = ? AND password_account = ?"
+    params = (username, password)
+    
+    user_data = execute_query(query, params, fetchone=True)
+
+    if user_data:
+        user = Incorect(*user_data[1:5])  # передайте только нужные элементы кортежа
+        return redirect(url_for('personal_cabinet', fio=user.FIO))
     else:
         session['error_message'] = 'Неверно введен логин или пароль'
         return redirect(url_for('index'))
 
-@app.route('/dashboard')
-def dashboard():
+@app.route('/personal_cabinet')
+def personal_cabinet():
     fio = request.args.get('fio', '')
-    return render_template('dashboard.html', fio=fio)
+    return render_template('personal_cabinet.html', fio=fio.encode('utf-8'))
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
